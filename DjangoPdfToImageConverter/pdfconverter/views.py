37b4fbd7c6e7ff340064234f674bc48b7725934e
kvarkson <kvarkson@gmail.com>
2013-08-28 00:58:18 +0300

    rebuild the database

--- DjangoPdfToImageConverter/pdfconverter/views.py
+++ DjangoPdfToImageConverter/pdfconverter/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from DjangoPdfToImageConverter.pdfconverter.forms import PdfUploadForm
 from DjangoPdfToImageConverter.pdfconverter.models import PdfFile
--- DjangoPdfToImageConverter/settings.py
+++ DjangoPdfToImageConverter/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pdffiles',                      # Or path to database file if using sqlite3.
+        'NAME': PROJECT_ROOT + '/assets/pdffiles_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
