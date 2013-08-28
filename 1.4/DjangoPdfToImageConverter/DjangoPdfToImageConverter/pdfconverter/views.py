64abf6448c36a8c909bcc0be88bf24480ebf813f
kvarkson <kvarkson@gmail.com>
2013-08-28 14:42:03 +0300

    added 1.4 version

---
+++ 1.4/DjangoPdfToImageConverter/DjangoPdfToImageConverter/pdfconverter/views.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from DjangoPdfToImageConverter.pdfconverter.forms import PdfUploadForm
+from DjangoPdfToImageConverter.pdfconverter.models import PdfFile
+
+def index(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = PdfUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_pdf = PdfFile(pdf_file = request.FILES['pdf_file'])
+            new_pdf.save()
+            new_pdf.convert_to_png()
+            new_pdf.convert_to_jpg()
+
+            return HttpResponseRedirect(reverse('DjangoPdfToImageConverter.pdfconverter.views.index'))
+    else:
+        form = PdfUploadForm()
+
+    pdf_files = PdfFile.objects.all()
+
+    return render_to_response(
+        'index.html',
+        {'pdf_files': pdf_files, 'form': form},
+        context_instance=RequestContext(request)
+    )
