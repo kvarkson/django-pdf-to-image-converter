2748d7124108f2fcb2581df287d2f5a496022248
kvarkson <kvarkson@gmail.com>
2013-08-28 14:41:58 +0300

    added 1.4 version

--- DjangoPdfToImageConverter/pdfconverter/forms.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django import forms
-
-class PdfUploadForm(forms.Form):
-    pdf_file = forms.FileField(
-        label='Select a pdf file',
-    )
--- DjangoPdfToImageConverter/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = patterns('',
-    url(r'^$', 'DjangoPdfToImageConverter.pdfconverter.views.index', name='index'),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
